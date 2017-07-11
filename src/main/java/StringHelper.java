5461e3bdf620f7f98b226bc11be0ac6e2334e6a6
adeshp16 <adeshp16@hawk.iit.edu>
2017-07-11 19:46:00 -0400

    adding string helper and pom file

---
+++ src/main/java/StringHelper.java
@@ -0,0 +1,43 @@
+
+public class StringHelper {
+
+	/**
+	 * 
+	 * @author ashish_admin
+	 * This method truncates letter A from first to positions if any.
+	 * @param str: input string
+	 * @return
+	 */
+	public String truncateAInFirst2Positions(String str) {
+		if (str.length() <= 2)
+			return str.replaceAll("A", "");
+
+		String first2Chars = str.substring(0, 2);
+		String stringMinusFirst2Chars = str.substring(2);
+
+		return first2Chars.replaceAll("A", "") 
+				+ stringMinusFirst2Chars;
+	}
+	
+	
+	/**
+	 * @author ashish_admin
+	 * This method checks if first and last 2 characters are same, if yes returns true
+	 * @param str
+	 * @return
+	 */
+	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+
+		if (str.length() <= 1)
+			return false;
+		if (str.length() == 2)
+			return true;
+
+		String first2Chars = str.substring(0, 2);
+
+		String last2Chars = str.substring(str.length() - 2);
+
+		return first2Chars.equals(last2Chars);
+	}
+	
+}
