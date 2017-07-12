e47eebada77966ac05fbddc36227d7ea0d17af5c
adeshp16 <adeshp16@hawk.iit.edu>
2017-07-11 20:28:03 -0400

    adding 2 test cases

--- src/test/java/edu/junit/helper/StringHelperTest.java
+++ src/test/java/edu/junit/helper/StringHelperTest.java
@@ -7,8 +7,22 @@ import org.junit.Test;
 public class StringHelperTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void truncate_test_1() {
+		
+		StringHelper helper=new StringHelper();
+		
+		//assertEquals(expectedString, actualString);
+		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	
+	@Test
+	public void truncate_test_2() {
+		
+		StringHelper helper=new StringHelper();
+		
+		//assertEquals(expectedString, actualString);
+		assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
 	}
 
 }
